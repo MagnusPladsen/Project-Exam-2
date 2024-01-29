75020194c84ba448bc416f800b934ab644e16e5c
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-01-29 11:05:29 +0100

    adjusted footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,14 @@
 import Logo from "../icons/logo/Logo";
 
-export const mobileFooterHeight = "60px";
-export const desktopFooterHeight = "80px";
+export const mobileFooterHeight = "200px";
+export const desktopFooterHeight = "200px";
 
 function Footer() {
   return (
     <nav
-      className={`h-[${mobileFooterHeight}] lg:h-[${desktopFooterHeight}] bg-primary`}
+      className={`px-4 h-[${mobileFooterHeight}] lg:h-[${desktopFooterHeight}] bg-primary`}
     >
-      <div className="flex items-center justify-center lg:justify-start lg:w-fit h-full gap-2">
+      <div className="flex items-start justify-start w-fit h-full gap-2">
         <Logo />
       </div>
     </nav>
