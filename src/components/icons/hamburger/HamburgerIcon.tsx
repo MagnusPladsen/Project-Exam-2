7fec57fb3c89c8a1b664b92ea107d879c175ca7f
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-01-29 11:05:15 +0100

    added svg hamburger icon

---
+++ src/components/icons/hamburger/HamburgerIcon.tsx
@@ -0,0 +1,21 @@
+function HamburgerIcon() {
+  return (
+    <svg
+      width="22"
+      height="18"
+      viewBox="0 0 22 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 41H41M1 21H41M1 1H41"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default HamburgerIcon;
