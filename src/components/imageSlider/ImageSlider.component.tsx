e3b11d7e0d028423e9ea0a80541e061364a981fc
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-02-15 21:01:37 +0100

    added max height and optional className prop

--- src/components/imageSlider/ImageSlider.component.tsx
+++ src/components/imageSlider/ImageSlider.component.tsx
@@ -1,15 +1,18 @@
-import { Carousel } from 'react-responsive-carousel';
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
-function ImageSlider({ images }: { images: string[] }) {
+function ImageSlider({
+  images,
+  className,
+}: {
+  images: string[];
+  className?: string;
+}) {
   return (
-    <Carousel>
+    <Carousel className={className}>
       {images.map((image, index) => (
         <div key={image}>
-          <img
-            src={image}
-            alt={`Venue Image ${index}`}
-          />
+          <img src={image} alt={`Venue Image ${index}`} className="max-h-[600px] lg:max-h-[800px] object-cover" />
         </div>
       ))}
     </Carousel>
