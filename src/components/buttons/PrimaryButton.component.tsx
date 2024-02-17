fa5d477e1e0e6298a0e797a50bda9ea42bf2ea9d
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-02-17 21:31:34 +0100

    added disabled state to button component

--- src/components/buttons/PrimaryButton.component.tsx
+++ src/components/buttons/PrimaryButton.component.tsx
@@ -6,7 +6,7 @@ function PrimaryButton({
   return (
     <button
       {...props}
-      className={`${className} w-fit py-3 px-5 bg-primary hover:bg-white border border-primary transition-all rounded-lg text-white hover:text-primary font-bold`}
+      className={`${className} disabled:bg-gray-300 disabled:text-gray-400 disabled:border-gray-400 w-fit py-3 px-5 bg-primary hover:bg-white border border-primary transition-all rounded-lg text-white hover:text-primary font-bold`}
     >
       {children}
     </button>
