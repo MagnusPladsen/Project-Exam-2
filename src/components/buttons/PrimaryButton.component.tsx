b68d8690c1c13314db417d3fd1754e2817bfe42a
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-02-17 21:29:09 +0100

    updated primaryButton to use normal button props for more dynamic use

--- src/components/buttons/PrimaryButton.component.tsx
+++ src/components/buttons/PrimaryButton.component.tsx
@@ -1,19 +1,12 @@
 function PrimaryButton({
   className,
-  type = "button",
-  onClick = () => {},
   children,
-}: {
-  className?: string;
-  type?: "button" | "submit";
-  onClick?: () => void;
-  children: React.ReactNode;
-}) {
+  ...props
+}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      type={type}
-      onClick={onClick}
-      className={`${className} w-fit py-4 px-6 bg-primary hover:bg-white border border-primary transition-all rounded-lg text-white hover:text-primary font-bold`}
+      {...props}
+      className={`${className} w-fit py-3 px-5 bg-primary hover:bg-white border border-primary transition-all rounded-lg text-white hover:text-primary font-bold`}
     >
       {children}
     </button>
