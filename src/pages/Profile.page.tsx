af9a2d3a104b266173bf75ce1d8114aec1c7ba4f
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-02-19 20:39:29 +0100

    WIP profile page

--- src/pages/Profile.page.tsx
+++ src/pages/Profile.page.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import H1 from "../components/common/H1.component";
 import H2 from "../components/common/H2.component";
 import Toggle from "../components/forms/Toggle.component";
@@ -12,7 +12,11 @@ import {
   useUpdateVenueManagerStatusMutation,
 } from "../services/api/holidazeApi";
 import { UpdateVenueManagerStatusRequest } from "../types/types";
+import PrimaryButton from "../components/buttons/PrimaryButton.component";
+import { AnimatePresence, motion } from "framer-motion";
 import H3 from "../components/common/H3.component";
+import capitalizeFirstLetter from "../formatters/capitalizeFirstLetter";
+import VenueCard from "../components/venue/VenueCard.component";
 
 function ProfilePage() {
   const { name } = useParams();
@@ -23,10 +27,12 @@ function ProfilePage() {
 
   const [updateStatus] = useUpdateVenueManagerStatusMutation();
 
-  const [venueManagerModalOpen, setVenueManagerModalOpen] = useState(false);
   const [userIsManager, setUserIsManager] = useState(
     data ? data.venueManager : false
   );
+
+  const [venueManagerModalOpen, setVenueManagerModalOpen] = useState(false);
+  const [openVenues, setOpenVenues] = useState(false);
 
   const toggleVenueManager = () => {
     setVenueManagerModalOpen(true);
@@ -65,7 +71,7 @@ function ProfilePage() {
   return (
     <>
       {data && !error && (
-        <article className="pt-[50px] lg:pt-[80px] antialiased bg-gradient-to-b from-primary to-white h-full w-full">
+        <article className="pt-[50px] lg:pt-[80px] antialiased bg-gradient-to-b from-primary to-white h-full w-full mb-40">
           <div className="w-full lg:w-[900px] px-4 mx-auto">
             <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg mt-20 lg:mt-24">
               <div className="px-6">
@@ -80,7 +86,7 @@ function ProfilePage() {
                             <img
                               src={data.avatar}
                               alt="Profile picture"
-                              className="shadow-xl rounded-full align-middle border-none absolute -my-16 left-1/2 transform -translate-x-1/2 max-w-[150px] h-[150px]"
+                              className="shadow-xl rounded-full align-middle absolute -my-16 left-1/2 transform -translate-x-1/2 max-w-[150px] h-[150px] border border-white"
                             />
                           ) : (
                             <ProfileIcon className="shadow-xl rounded-full h-auto align-middle border-none absolute -my-16 left-1/2 transform -translate-x-1/2 !max-w-[150px]" />
@@ -106,7 +112,7 @@ function ProfilePage() {
                       <H2
                         className={`!text-primary text-xs  leading-normal mb-4 uppercase`}
                       >
-                        Venue Tenant
+                        {data.venueManager ? "Venue Manager" : "Venue Tenant"}
                       </H2>
                     )}
 
@@ -134,22 +140,43 @@ function ProfilePage() {
                     )}
                   </div>
                 </div>
-                <div className="mt-2 py-10 border-t border-blueGray-200 text-center">
-                  <div className="flex flex-wrap justify-center">
-                    <div className="w-full lg:w-9/12 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-blueGray-700">
-                        An artist of considerable range, Jenna the name taken by
-                        Melbourne-raised, Brooklyn-based Nick Murphy writes,
-                        performs and records all of his own music, giving it a
-                        warm, intimate feel with a solid groove structure. An
-                        artist of considerable range.
-                      </p>
-                      <a href="#" className="font-normal text-pink-500">
-                        Show more
-                      </a>
-                    </div>
+                {userIsManager && (
+                  <div className="mt-2 py-10 border-t border-blueGray-200 text-center">
+                    <PrimaryButton
+                      className="mb-5"
+                      onClick={() => setOpenVenues((prev) => !prev)}
+                    >
+                      {openVenues ? "Hide users venues" : "See users venues"}
+                    </PrimaryButton>
+                    <AnimatePresence initial={false}>
+                      {openVenues && (
+                        <motion.div
+                          initial={{ opacity: 0, height: 0 }}
+                          animate={{ opacity: 1, height: "auto" }}
+                          exit={{ opacity: 0, height: 0 }}
+                          transition={{ duration: 0.3 }}
+                          className="mt-5 flex flex-col gap-4 lg:max-w-[500px] lg:mx-auto"
+                        >
+                       {/*    {data.venues?.map((venue) => (
+                            <Link key={venue.id} to={`/venue/${venue.id}`}>
+                              <div className="flex flex-col w-full items-center gap-2 bg-white rounded-lg border border-gray-200 shadow-md py-8">
+                                <H3 className="font-bold underline underline-offset-2 ">
+                                  {capitalizeFirstLetter(venue.name)}
+                                </H3>
+                                <p className="text-xs">
+                                  {capitalizeFirstLetter(venue.description)}
+                                </p>
+                              </div>
+                            </Link>
+                          ))} */}
+                          {data.venues?.map((venue) => (
+                            <VenueCard key={venue.id} venue={venue!} />
+                          ))}
+                        </motion.div>
+                      )}
+                    </AnimatePresence>
                   </div>
-                </div>
+                )}
               </div>
             </div>
           </div>
