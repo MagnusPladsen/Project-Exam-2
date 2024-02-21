32a6cd4727062dd3910cb0941806a334ac658fbd
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-02-21 17:34:50 +0100

    validation is done, changed input field to support all types, moved errorIcon to own component

--- src/components/forms/Input.component.tsx
+++ src/components/forms/Input.component.tsx
@@ -1,35 +1,44 @@
-import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 import FormErrorMessage from "../messages/FormErrorMessage.component";
 import { InputProps } from "../../types/types";
 
 function Input({ name, label, className, ...inputProps }: InputProps) {
   const {
+    control,
     register,
     formState: { errors },
   } = useFormContext();
   return (
-    <div className="flex flex-col w-full gap-2">
-      {!!label && (
-        <label
-          htmlFor={name}
-          className={` ${
-            errors[name] && "!text-red-500 "
-          } text-left text-gray-500`}
-        >
-          {label}
-        </label>
-      )}
-      <input
-        {...register(name)}
-        {...inputProps}
-        name={name}
-        id={name}
-        className={`${
-          errors[name] && "!border-red-500 "
-        } ${className} appearance-none border-2 border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 shadow-inner focus:ring-primary focus:shadow-lg checked:bg-primary checked:border-primary checked:text-white`}
-      />
-      <FormErrorMessage name={name} />
-    </div>
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState }) => {
+        return (
+          <div className="flex flex-col w-full gap-2">
+            {!!label && (
+              <label
+                htmlFor={name}
+                className={` ${
+                  errors[name] && "!text-red-500 "
+                } text-left text-gray-500`}
+              >
+                {label}
+              </label>
+            )}
+            <input
+              {...field}
+              {...inputProps}
+              name={name}
+              id={name}
+              className={`${
+                (errors[name] || fieldState.invalid) && "!border-red-500 "
+              } ${className} appearance-none border-2 border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 shadow-inner focus:ring-primary focus:shadow-lg checked:bg-primary checked:border-primary checked:text-white`}
+            />
+            <FormErrorMessage name={name} />
+          </div>
+        );
+      }}
+    />
   );
 }
 
--- src/components/modals/venue/validation.ts
+++ src/components/modals/venue/validation.ts
@@ -9,10 +9,21 @@ const schema = yup.object().shape({
     .string()
     .required("Description is required")
     .min(3, "Description must be at least 3 characters"),
-  price: yup.number().required("Price is required"),
-  maxGuests: yup.number().required("Max guests is required"),
+  price: yup
+    .number()
+    .typeError("Price must be a number")
+    .required("Price is required")
+    .min(0, "Price must be at least 0"),
+  maxGuests: yup
+    .number()
+    .typeError("Max guests must be a number")
+    .required("Max guests is required"),
   media: yup.string(),
-  rating: yup.number().max(5, "Rating must be between 0 and 5").min(0),
+  rating: yup
+    .number()
+    .typeError("Rating must be a number")
+    .max(5, "Rating must be between 0 and 5")
+    .min(0, "Rating must be between 0 and 5"),
   wifi: yup.boolean(),
   parking: yup.boolean(),
   breakfast: yup.boolean(),
