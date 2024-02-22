b72a56042639d5e5bf6d5c62683713f37a51e8ae
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-02-22 20:30:19 +0100

    fixed spacing

--- src/pages/Profile.page.tsx
+++ src/pages/Profile.page.tsx
@@ -104,7 +104,7 @@ function ProfilePage() {
         <article className="pt-[50px] lg:pt-[80px] antialiased bg-gradient-to-b from-primary to-white h-full w-full pb-40 shadow-md">
           <div className="w-full lg:w-[900px] lg:px-4 mx-auto">
             <div className=" relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl lg:rounded-lg mt-20 lg:mt-24">
-              <div className="lg:px-6">
+              <div className="lg:px-6 mb-10">
                 <div className="flex flex-wrap justify-center">
                   <div className="px-4 flex justify-center">
                     <div className="relative">
