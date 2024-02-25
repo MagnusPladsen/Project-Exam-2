84dde006f2af71c25441e71f313b657ea5441af3
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-02-25 16:07:38 +0100

    fixed datepicker type

--- src/components/HolidazeDatePicker/HolidazeDatePicker.component.tsx
+++ src/components/HolidazeDatePicker/HolidazeDatePicker.component.tsx
@@ -1,7 +1,10 @@
-import DatePicker from "@sentisso/react-modern-calendar-datepicker";
+import DatePicker, {
+  RenderInputProps,
+} from "@sentisso/react-modern-calendar-datepicker";
 import "@sentisso/react-modern-calendar-datepicker/lib/DatePicker.css";
 import { HolidazeDatePickerProps } from "../../types/types";
 import H3 from "../common/H3.component";
+import { RefObject } from "react";
 
 function HolidazeDatePicker({
   value,
@@ -10,11 +13,11 @@ function HolidazeDatePicker({
   inputPlaceholder,
   ...otherProps
 }: HolidazeDatePickerProps) {
-  const holidazeStyleInput = ({ ref }: any) => (
+  const holidazeStyleInput = ({ ref }: RenderInputProps) => (
     <div className="flex flex-col gap-2 w-full lg:w-52">
       {inputLabel && <H3>{inputLabel}</H3>}
       <input
-        ref={ref}
+        ref={ref as RefObject<HTMLInputElement>}
         className={`appearance-none border-2 border-gray-100 rounded-lg px-4 py-3 placeholder-gray-300 focus:outline-none focus:ring-2 hover:border-primary focus:ring-primary focus:shadow-lg w-full cursor-pointer hover:placeholder:text-primary`}
         placeholder={inputPlaceholder ?? "Select a date"}
       />
