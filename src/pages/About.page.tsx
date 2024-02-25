e91a011769e86bd26579a4c60e26f0711d65e00b
Magnus Pladsen <magnus_pladsen@hotmail.com>
2024-02-25 11:44:38 +0100

    fixed text on about page

--- src/pages/About.page.tsx
+++ src/pages/About.page.tsx
@@ -1,17 +1,19 @@
 import AboutLinks from "../components/about/AboutLinks.component";
 import TechStack from "../components/about/AboutTechStack.component";
 import H1 from "../components/common/H1.component";
+import H2 from "../components/common/H2.component";
 
 function AboutPage() {
   return (
     <section className="min-h-[100vh] pt-[80px] lg:pt-[120px] mx-auto pb-16 lg:px-6">
       <div className="mx-auto lg:max-w-screen-sm text-center mb-8 flex flex-col w-[90vw] lg:min-w-[900px] lg:w-[900px]">
-        <div className="flex flex-col mb-10 lg:mb-20">
-          <H1>About Holidaze</H1>
+        <div className="flex flex-col mb-10 lg:mb-20 max-w-[600px] mx-auto">
+          <H1 className="!mb-10">About</H1>
+          <H2 className="!text-lg mb-3">
+            Introducing <span className="text-primary">Holidaze</span>
+          </H2>
           <p>
-            Introducing <span className="text-primary">Holidaze</span>: A
-            fictional project crafted as the crowning achievement of my Noroff
-            Front-End Development course, powered by the Noroff API.
+            A fictional web application designed as the culminating project for my final exam in the Noroff Front-End Development course, driven by the Noroff v1 API.
           </p>
         </div>
 
